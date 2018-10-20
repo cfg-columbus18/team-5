86146e25c659fb7d2af1d98b86e9b9850701d369
Stephen Horning <Stephen@Stephens-MacBook-Pro.local>
2018-10-20 01:05:28 -0400

    Updates Model

---
+++ mentorship/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/admin/github/team-5/mentorship/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- mentorship/matching/models.py
+++ mentorship/matching/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Language(models.Model):
+    language_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.language_name
+
+class SponsorComponent(models.Model):
+    sponsor_component = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sponsor_component
+
+class CommunicationPlatform(models.Model):
+    communication_platform = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.communication_platform
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    province_state_region = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    timezone = models.CharField(max_length=50)
+    communication_platforms = models.ManyToManyField(CommunicationPlatform)
+    sponsor_stage = models.CharField(max_length=50)
+    languages = models.ManyToManyField(Language)
+    sponsor_components = models.ManyToManyField(SponsorComponent)
+    faith = models.CharField(max_length=50)
+    sponsor_type = models.CharField(max_length=50)
+    refugee_region = models.CharField(max_length=50)
