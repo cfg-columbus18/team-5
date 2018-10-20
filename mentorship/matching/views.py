6d6eb1495738f2306ce425a0352ca542a09cb374
forsman5 <jrforsman@gmail.com>
2018-10-19 20:59:05 -0400

    added basic homepage

--- mentorship/matching/views.py
+++ mentorship/matching/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(req):
-    return HttpResponse("Hello, world.")
+    template = loader.get_template('index.html')
+    context = {
+        'x': range(5),
+    }
+
+    return HttpResponse(template.render(context, req))
