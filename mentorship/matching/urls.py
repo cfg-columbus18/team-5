dace656412d5fefa8848a0c0e8d712c0c54763c1
forsman5 <jrforsman@gmail.com>
2018-10-19 22:34:35 -0400

    basic user signing up

--- mentorship/matching/urls.py
+++ mentorship/matching/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutView, name='logout'),
     path('register/', views.register, name='register'),
 ]
--- mentorship/matching/views.py
+++ mentorship/matching/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(req):
@@ -11,14 +13,25 @@ def index(req):
 
     return HttpResponse(template.render(context, req))
 
-def logout(req):
+def logoutView(req):
 
     return HttpResponse("logout")
 
-def login(req):
+def loginPage(req):
 
     return HttpResponse("login")
 
 def register(req):
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(req, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
 
-    return HttpResponse("register")
+    return render(req, 'register.html', {'form': form})
